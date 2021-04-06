b7a1c5124580f103c82b8c82660dbcf22da61945
joaochiva <49000270+joaochiva@users.noreply.github.com>
2021-04-06 19:46:52 +0100

    Add files via upload

---
+++ php/index_teste.php
@@ -0,0 +1,81 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tic Tac Toe</title>
+    <!--<link rel="stylesheet" href="../css/style2.css">-->
+    <!--<link href="../docs/5.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">-->
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/navbars/">
+    <!-- separação das cenas novas -->
+    
+    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">   
+    <link rel="stylesheet" type="text/css" href="../teste_novo/public/css/bootstrap.min.css"><!--
+		<link rel="stylesheet" type="text/css" href="../teste_novo/public/css/style.css">
+		<link rel="stylesheet" type="text/css" href="../teste_novo/public/css/animate.css">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+    
+		<link href="../teste_novo/public/apple-icon/icon57X57.png" rel="apple-touch-icon" />
+		<link href="../teste_novo/public/apple-icon/icon76X76.png" rel="apple-touch-icon" sizes="76x76" />
+		<link href="../teste_novo/public/apple-icon/icon120X120.png" rel="apple-touch-icon" sizes="120x120" />
+		<link href="../teste_novo/public/apple-icon/icon152X152.png" rel="apple-touch-icon" sizes="152x152" />
+		<link href="../teste_novo/public/apple-icon/icon180X180.png" rel="apple-touch-icon" sizes="180x180" />
+		<link href="../teste_novo/public/android-icon/icon192X192.png" rel="icon" sizes="192x192" />
+		<link href="../teste_novo/public/android-icon/icon192X192.png" rel="icon" sizes="128x128" />-->
+
+    
+    
+    <!-- Bootstrap core CSS -->
+    <!--<link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">-->
+    <link rel="stylesheet" href="../css/style2.css">
+    <link rel="stylesheet" type="text/css" href="../teste_novo/public/css/style.css">
+		<link rel="stylesheet" type="text/css" href="../teste_novo/public/css/animate.css">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+
+    <link href="../teste_novo/public/apple-icon/icon57X57.png" rel="apple-touch-icon" />
+		<link href="../teste_novo/public/apple-icon/icon76X76.png" rel="apple-touch-icon" sizes="76x76" />
+		<link href="../teste_novo/public/apple-icon/icon120X120.png" rel="apple-touch-icon" sizes="120x120" />
+		<link href="../teste_novo/public/apple-icon/icon152X152.png" rel="apple-touch-icon" sizes="152x152" />
+		<link href="../teste_novo/public/apple-icon/icon180X180.png" rel="apple-touch-icon" sizes="180x180" />
+		<link href="../teste_novo/public/android-icon/icon192X192.png" rel="icon" sizes="192x192" />
+		<link href="../teste_novo/public/android-icon/icon192X192.png" rel="icon" sizes="128x128" />
+    
+    <!--<style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>-->
+
+    
+    
+  </head>
+  <body>
+  <?php include('navbar.php'); ?>
+    
+<?php include('../teste_novo/views/index.html'); ?> 
+   
+
+<footer>
+<?php include('footer.php'); ?>
+</footer>
+
+<script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+<script src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-beta1/jquery.min.js"></script>
+</body>
+
+
+</html>
